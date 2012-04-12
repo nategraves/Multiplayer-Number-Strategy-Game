592449bc9004df9d8fd0df2d61e3c83798e29d91
natesgraves <popephatt@hotmail.com>
2012-04-12 01:56:33 -0400

    Absolutely stumped as to why the post is not working

--- web.py
+++ web.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import jsonify
+from company import *
 
 app = Flask(__name__)
 
@@ -8,14 +10,27 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@app.route('/new', methods=['POST', 'GET'])
+@app.route('/new', methods=['POST'])
 def new_game():
 	error = None
 	if request.method == 'POST':
-		pass #need to hand off to another method
-		return('New game')
+		print(request.data)
+		try: 
+			width = request.form['width']
+		except KeyError:
+			print("Bad")
+		#height = request.form['height']
+		#player1 = request.form['player1']
+		#player2 = request.form['player2']
+		#print(width, height, player1, player2)
+		return render_template('game.html')
 	else:
-		return render_template('new.html')
+		error = 'Wrong'
+
+@app.route('/play/<tile>')
+def play():
+	play_tile(tile)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.debug = True
+	app.run()
